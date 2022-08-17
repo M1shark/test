96f6ecfb03313dd8c8068fddf684f4399946aa03
Михаил <88690428+M1shark@users.noreply.github.com>
2022-08-17 23:36:16 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,9 @@
 $('.hero__slider').slick({
     arrows: false,
-    //autoplay: true,
-    autoplaySpeed: 4000,
-    infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-
 });
+
 $('.discover__card').slick({
     arrows: false,
     infinite: true,
@@ -41,30 +38,3 @@ burger.addEventListener('click', function() {
 });
 
 //select
-jQuery(($) => {
-    $('.select').on('click', '.select__head', function() {
-        if ($(this).hasClass('open')) {
-            $(this).removeClass('open');
-            $(this).next().fadeOut();
-        } else {
-            $('.select__head').removeClass('open');
-            $('.select__list').fadeOut();
-            $(this).addClass('open');
-            $(this).next().fadeIn();
-        }
-    });
-
-    $('.select').on('click', '.select__item', function() {
-        $('.select__head').removeClass('open');
-        $(this).parent().fadeOut();
-        $(this).parent().prev().text($(this).text());
-        $(this).parent().prev().prev().val($(this).text());
-    });
-
-    $(document).click(function(e) {
-        if (!$(e.target).closest('.select').length) {
-            $('.select__head').removeClass('open');
-            $('.select__list').fadeOut();
-        }
-    });
-});
